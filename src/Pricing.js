58a579508072832597cd37662cac11f8414dd923
Adam Mannino <amannino92@gmail.com>
2024-04-02 13:06:14 -0400

    Pricing Page Components Added

--- src/Pricing.js
+++ src/Pricing.js
@@ -1,16 +1,62 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './Pricing.css';
 
-const Pricing = () => {
-    return (
-<>
-    
-    <div className='pricing-page'>
-        
-    </div>
+function Pricing({ onButtonClick }) {
+    const handleButtonClick = () => {
+        onButtonClick();
+    };
 
-</>
+    return (
+        <>
+            <div className='pricing-page'>
+                <div className='pricing-container'>
+                    <h1 className='pricing-header'>
+                        PRICING
+                    </h1>
+                    <ul>
+                        <li>
+
+                        </li>
+
+                    </ul>
+
+                    <div className='pricing-deals-one'>
+                        <h2>BASIC</h2>
+                        <h1 className='price-one'>
+                            $49.99/mo
+                        </h1>
+                        <ul>
+                            <li>One</li>
+                            <li>Two</li>
+                            <li>Three</li>
+                            <li>Four</li>
+                        </ul>
+                    </div>
+                    <div className='pricing-deals-two'>
+                        <h2>Pro</h2>
+                        <h1 className='price-two'>
+                            $79.99/mo
+                        </h1>
+                        <ul>
+                            <li>One</li>
+                            <li>Two</li>
+                            <li>Three</li>
+                            <li>Four</li>
+                        </ul>
+                        <p>
+                            <Link to="/pricing" className="trial-button" onClick={handleButtonClick}>Start-Trial</Link>
+                            <Link to="/pricing" className="week-free-button" onClick={handleButtonClick}>1-Week Access</Link>
+
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+        </>
     )
 }
+
 
 export default Pricing
--- src/Schedules.js
+++ src/Schedules.js
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
 import './Schedules.css';
+import 'react-calendar/dist/Calendar.css';
 
-const Schedules = () => {
+function Schedules() {
+  const [date, setDate] = useState(new Date());
+
+  const handlePrevMonth = () => {
+    const prevMonth = new Date(date.getFullYear(), date.getMonth() - 1);
+    setDate(prevMonth);
+  };
+
+  const handleNextMonth = () => {
+    const nextMonth = new Date(date.getFullYear(), date.getMonth() + 1);
+    setDate(nextMonth);
+  };
+
   return (
-<>
-    
     <div className='schedules-page'>
       <div className='schedules-container'>
-
-
-
+        <hr className='horizontal-line'></hr>
+        <div className='calendar'>
+          <Calendar
+            value={date}
+            onChange={setDate}
+          />
+          </div>
+        </div>
       </div>
-    </div>
-
-</>
-  )
+  );
 }
 
-export default Schedules
+export default Schedules;
