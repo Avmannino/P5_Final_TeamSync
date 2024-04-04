30e7d379e772db28fbc300fd263190d07f1d1542
Adam Mannino <amannino92@gmail.com>
2024-04-04 16:53:58 -0400

    Formatting for all pages

--- src/Navbar.js
+++ src/Navbar.js
@@ -13,7 +13,17 @@ const Navbar = () => {
       </div>
       <div className="nav-links">
         <Link to="/home" className="nav-link">Home</Link>
-        <Link to="/sports" className="nav-link">Sports</Link>
+        <div className="nav-link dropdown">
+          Sports
+          <div className="dropdown-content">
+            <Link to="/sports/baseball">Baseball</Link>
+            <Link to="/sports/football">Football</Link>
+            <Link to="/sports/soccer">Soccer</Link>
+            <Link to="/sports/icehockey">Ice Hockey</Link>
+            <Link to="/sports/basketball">Basketball</Link>
+            <Link to="/sports/tennis">Tennis</Link>
+          </div>
+        </div>
         <Link to="/schedules" className="nav-link">Schedules</Link>
         <Link to="/pricing" className="nav-link">Pricing</Link>
         <Link to="/social" className="nav-link">Social</Link>
